06140452f39fa0239e631db29461258a6b1cdb7f
alexisp <contact@alexis-petit.fr>
2018-05-22 13:03:15 +0200

    Generate grid

--- scripts/minesweeper.js
+++ scripts/minesweeper.js
@@ -1,12 +1,54 @@
-const ROW = 9, COLUMNS = 9, MINES = 10;
+const ROWS = 9, COLUMNS = 9, MINES = 10;
 const GRID = [];
 
 let initModel = (rows, columns, mines) => {
+  let columnsTemp = [], minesTemp = [];
 
+  for (var m = 0; m < mines; m++) {
+    let randomRow = Math.floor(Math.random() * Math.floor(ROWS));
+    let randomCol = Math.floor(Math.random() * Math.floor(COLUMNS));
+    minesTemp.push([randomRow,randomCol]);
+  }
+  console.log('array mines : ', minesTemp);
+
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+
+      var isMine = $.grep(minesTemp, function(element, index){
+        return element[0] == r && element[1] == c
+      });
+
+      columnsTemp.push(isMine.length ? true : false)
+    }
+    GRID.push(columnsTemp);
+    columnsTemp = [];
+  }
+  // console.log('array grid : ',GRID);
+}
+
+let displayGrid = () => {
+  $('#ms-box').html('<div id="ms-grid"></div>');
+  let htmlData = '';
+  htmlData += '<table>';
+
+  for (let r = 0; r < GRID.length; r++) {
+    htmlData += '<tr>';
+    for (let c = 0; c < GRID[r].length; c++) {
+      console.log();
+      htmlData += '<td>';
+      htmlData += GRID[r][c] ? 'X' : '-';
+      htmlData += '</td>';
+    }
+    htmlData += '</tr>';
+  }
+
+
+  $('#ms-grid').html(htmlData);
 }
 
 let startGame = () => {
-  initModel(ROW,COLUMNS,MINES);
+  initModel(ROWS,COLUMNS,MINES);
+  displayGrid();
 }
 
 startGame();
