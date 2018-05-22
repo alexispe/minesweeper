8459312a384cdfb4844905791b3c0e85c4222e93
alexisp <contact@alexis-petit.fr>
2018-05-22 11:19:51 +0200

    Initial Arch

---
+++ scripts/minesweeper.js
@@ -0,0 +1,44 @@
+const ROW = 9, COLUMNS = 9, MINES = 10;
+const GRID = [];
+
+let initModel = (rows, columns, mines) => {
+
+}
+
+let startGame = () => {
+  initModel(ROW,COLUMNS,MINES);
+}
+
+startGame();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let createMap = (height,width) => {
+//   const map = [];
+//   let html = '';
+//   console.log(height,width);
+//   for (let h = 0; h < height; h++) {
+//     for (let w = 0; w < width; w++) {
+//       $('.map').append('<div></div>');
+//     }
+//   }
+//   console.log(html);
+// }
+//
+// createMap(10,20)
+// // console.log('kqgsvhjbn');
+// //
+// // console.log($('.map'));
+// // $('.map').addClass('yo')
